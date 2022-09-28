d9b2ace168c1964aa280da15cf04d2a8e4180e68
influous <influous@mailbox.org>
2022-09-28 17:35:59 +0200

    Typo

--- src/com.influous/Docker.groovy
+++ src/com.influous/Docker.groovy
@@ -11,8 +11,8 @@ class Docker implements Serializable {
 
     def buildDockerImage(String imageName) {
         script.echo 'Building Docker image'
-        script.withCredentials[usernamePassword(script.credentialsId: 'docker-hub-repo', script.usernameVariable: 'USER', script.passwordVariable: 'PASSWORD')]) {
-            script.sh "docker build -t $imageName . "
+        script.withCredentials[script.usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASSWORD')]) {
+            script.sh "docker build -t $imageName ."
             script.sh "echo '${script.PASSWORD}' | docker login -u $script.USER --password-stdin"
             script.sh "docker push $imageName"
         }
