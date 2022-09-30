0e796bd734d5c88f81e467a4f9455d3f0d0bd573
influous <influous@mailbox.org>
2022-09-30 16:18:09 +0200

    Include version number & purge

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
 def call() {
-    echo "Building the application (JAR) for branch v${env.BRANCH_NAME}..."
-    sh 'mvn package'
+    echo "Building the application (JAR) v${params.VERSION}..."
+    sh 'mvn clean package'
 }
