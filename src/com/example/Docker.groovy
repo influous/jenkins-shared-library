e339b19629453c59211e7d66245c2b6f0669b64e
influous <influous@mailbox.org>
2022-09-30 16:56:38 +0200

    Update Groovy script

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -11,7 +11,7 @@ class Docker implements Serializable {
 
     def buildImage(String imageName) {
         script.echo 'Building Docker image'
-        script.withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASSWORD')]) {
+        withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASSWORD')]) {
         script.sh "docker build -t $imageName ."
         script.sh "echo '${PASSWORD}' | docker login -u $USER --password-stdin"
         script.sh "docker push $imageName"
