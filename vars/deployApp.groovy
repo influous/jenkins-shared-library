a199dbad88d5f808481249d6ba728da366b4c767
influous <influous@mailbox.org>
2022-09-30 19:14:59 +0200

    Update ports

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -2,7 +2,7 @@
 
 def call() {
     echo "Deploying to branch ${env.BRANCH_NAME}"
-    def dockerCmd = "docker run -d -p 3000:80 ${IMAGE_NAME}"
+    def dockerCmd = "docker run -d -p 8080:8080 ${IMAGE_NAME}"
     sshagent(['ec2-ssh-key']) {
     // -o flag avoids SSH popup
     sh "ssh -o StrictHostKeyChecking=no ${EC2_USER}@${EC2_ADDRESS} ${dockerCmd}"
