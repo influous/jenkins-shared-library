653b2ca556c9493fb978ed523109f01370c04008
influous <influous@mailbox.org>
2022-10-01 19:04:02 +0200

    Typo

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -2,10 +2,10 @@
 
 def call() {
     echo "Deploying to branch ${env.BRANCH_NAME}"
-    def shellCmds = "bash ./server-cmds.sh"
+    def shellCmds = "bash ./server_cmds.sh"
     sshagent(['ec2-ssh-key']) {
     // -o flag avoids SSH popup
-    sh "scp -o StrictHostKeyChecking=no server-cmds.sh ${EC2_USER}@${EC2_ADDRESS}:/home/ubuntu"
+    sh "scp -o StrictHostKeyChecking=no server_cmds.sh ${EC2_USER}@${EC2_ADDRESS}:/home/ubuntu"
     sh "scp -o StrictHostKeyChecking=no docker-compose.yaml ${EC2_USER}@${EC2_ADDRESS}:/home/ubuntu"
     sh "ssh -o StrictHostKeyChecking=no ${EC2_USER}@${EC2_ADDRESS} ${shellCmds}"
     }
