9be605a4636633fa2d72340300201aca050f4fee
influous <influous@mailbox.org>
2022-10-01 18:09:20 +0200

    Fix scp transfer

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -4,7 +4,7 @@ def call() {
     echo "Deploying to branch ${env.BRANCH_NAME}"
     def dockerCmd = "docker-compose -f docker-compose.yaml up --detach"
     sshagent(['ec2-ssh-key']) {
-    sh "scp docker-compose.yaml ${EC2_USER}@${EC2_ADDRESS}:/home/ubuntu"
+    sh "scp -o StrictHostKeyChecking=no docker-compose.yaml ${EC2_USER}@${EC2_ADDRESS}:/home/ubuntu"
     // -o flag avoids SSH popup
     sh "ssh -o StrictHostKeyChecking=no ${EC2_USER}@${EC2_ADDRESS} ${dockerCmd}"
     }
