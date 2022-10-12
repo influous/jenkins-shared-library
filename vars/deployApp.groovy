ab6c94d6729096584c974c722f2be8add6069950
Toby Kook <toby@kook.work>
2022-10-12 17:13:58 +0200

    Add nginx to cluster

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -9,4 +9,15 @@ def call() {
     sh "scp -o StrictHostKeyChecking=no docker-compose.yaml ${EC2_USER}@${EC2_ADDRESS}:/home/ubuntu"
     sh "ssh -o StrictHostKeyChecking=no ${EC2_USER}@${EC2_ADDRESS} ${shellCmds}"
     }
+
+    when {
+        expression {
+            env.BRANCH_NAME == 'feature/deploy-on-k8s'
+            try {
+                sh "kubectl create deployment nginx-deployment --image=nginx"
+            } catch (Expection e) {
+                echo "Exception: " + e.toString()
+            }
+        }
+    }
 }
