226b038f3be6daf1103b3cccae643770a1b1cfe8
influous <influous@mailbox.org>
2022-10-17 15:49:48 +0200

    Purge cached Terraform files before push

--- vars/updateGit.groovy
+++ vars/updateGit.groovy
@@ -5,6 +5,7 @@ def call() {
     sh 'git config --global user.email "jenkins@kook.work"'
     sh 'git config --global user.name "jenkins"'
     sh "git remote set-url origin https://${PASSWORD}@github.com/${USER}/devops-maven.git"
+    sh "git filter-branch -f --index-filter 'git rm --cached -r --ignore-unmatch .terraform/'"
     sh 'git add .'
     sh 'git commit -m "CI: Version bump"'
     echo "Pushing to $env.BRANCH_NAME..."
